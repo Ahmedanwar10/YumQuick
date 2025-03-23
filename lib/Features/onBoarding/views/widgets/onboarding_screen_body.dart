7f78dbfa91e73fe3b4a3c015538591ebe01fde91
Ahmed Anwar <01125474900ahmed@gmail.com>
2025-03-23 21:40:37 +0200

    onBoarding Done

--- lib/Features/onBoarding/views/widgets/onboarding_screen_body.dart
+++ lib/Features/onBoarding/views/widgets/onboarding_screen_body.dart
@@ -1,117 +1,170 @@
 import 'package:flutter/material.dart';
-import 'package:introduction_screen/introduction_screen.dart';
 import 'package:yum_quick/Features/home/presentation/views/home_view.dart';
 import 'package:yum_quick/core/constants/assets.dart';
+import 'package:yum_quick/core/resources/color_managers.dart';
 
-class OnBoardingScreenBody extends StatelessWidget {
-  OnBoardingScreenBody({super.key});
+class OnBoardingScreenBody extends StatefulWidget {
+  const OnBoardingScreenBody({super.key});
 
-  final List<PageViewModel> pages = [
-    PageViewModel(
-      titleWidget: SizedBox.shrink(), // إزالة العنوان من الموضع الافتراضي
-      bodyWidget: SizedBox.shrink(), // إزالة النص من الموضع الافتراضي
-      image: SizedBox.shrink(),
-      decoration: PageDecoration(
-        boxDecoration: BoxDecoration(
-          color: Colors.transparent,
-        ),
-      ),
-    ),
-       PageViewModel(
-      titleWidget: SizedBox.shrink(), // إزالة العنوان من الموضع الافتراضي
-      bodyWidget: SizedBox.shrink(), // إزالة النص من الموضع الافتراضي
-      image: SizedBox.shrink(),
-      decoration: PageDecoration(
-        boxDecoration: BoxDecoration(
-          color: Colors.transparent,
-        ),
-      ),
-    ),
-       PageViewModel(
-      titleWidget: SizedBox.shrink(), // إزالة العنوان من الموضع الافتراضي
-      bodyWidget: SizedBox.shrink(), // إزالة النص من الموضع الافتراضي
-      image: SizedBox.shrink(),
-      decoration: PageDecoration(
-        boxDecoration: BoxDecoration(
-          color: Colors.transparent,
-        ),
-      ),
-    ),
+  @override
+  State<OnBoardingScreenBody> createState() => _OnBoardingScreenBodyState();
+}
+
+class _OnBoardingScreenBodyState extends State<OnBoardingScreenBody> {
+  int currentIndex = 0;
+
+  final List<Map<String, dynamic>> pages = [
+    {
+      "title": "Order For Food",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      "icon": Icons.outbox_rounded,
+      "image": Assets.imagesOnboardingA,
+    },
+    {
+      "title": "Fast Delivery",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      "icon": Icons.payment,
+      "image": Assets.imagesOnboardingB,
+    },
+    {
+      "title": "Enjoy Your Meal",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      "icon": Icons.fastfood,
+      "image": Assets.imagesOnboardingC,
+    },
   ];
+
+  void nextPage() {
+    if (currentIndex < pages.length - 1) {
+      setState(() {
+        currentIndex++;
+      });
+    } else {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (_) => const HomeView()),
+      );
+    }
+  }
+
+  void previousPage() {
+    if (currentIndex > 0) {
+      setState(() {
+        currentIndex--;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Positioned.fill(
-          child: Image.asset(
-            Assets.imagesOnboardingA,
-            fit: BoxFit.cover,
-          ),
-        ),
-        Column(
+    return Scaffold(
+      body: GestureDetector(
+        onHorizontalDragEnd: (details) {
+          if (details.primaryVelocity! < -50) {
+            nextPage();
+          } else if (details.primaryVelocity! > 50) {
+            previousPage();
+          }
+        },
+        child: Stack(
           children: [
-            Spacer(),
-            Container(
-              width: double.infinity,
-              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30),
-                  topRight: Radius.circular(30),
-                ),
+            Positioned.fill(
+              child: Image.asset(
+                pages[currentIndex]["image"],
+                fit: BoxFit.cover,
               ),
-              child: Column(
-                children: [
-                  Icon(Icons.fastfood, size: 50, color: Colors.orange),
-                  SizedBox(height: 10),
-                  Text(
-                    "Order For Food",
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.orange),
-                  ),
-                  SizedBox(height: 10),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 10),
-                    child: Text(
-                      "Lorem ipsum dolor sit amet, conse ctetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 16, color: Colors.black54),
+            ),
+
+            Column(
+              children: [
+                const Spacer(),
+                Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
                     ),
                   ),
-                  SizedBox(height: 20),
-                  Container(
-                    padding: EdgeInsets.symmetric(vertical: 12, horizontal: 40),
-                    decoration: BoxDecoration(
-                      color: Colors.orange,
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: Text(
-                      "Next",
-                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
-                    ),
+                  child: Column(
+                    children: [
+                      Icon(
+                        pages[currentIndex]["icon"],
+                        size: 50,
+                        color:ColorManager.primary,
+                      ),
+                      const SizedBox(height: 10),
+
+                      // العنوان
+                      Text(
+                        pages[currentIndex]["title"]!,
+                        style: const TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color:ColorManager.primary,
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+
+
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        child: Text(
+                          pages[currentIndex]["description"]!,
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(fontSize: 16, color: Colors.black54),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+
+                      GestureDetector(
+                        onTap: nextPage,
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 40),
+                          decoration: BoxDecoration(
+                            color: ColorManager.primary,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Text(
+                            currentIndex == pages.length - 1 ? "Get Started" : "Next",
+                            style: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
+                ),
+              ],
+            ),
+
+            // زر تخطي
+            Positioned(
+              top: 50,
+              right: 20,
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(builder: (_) => const HomeView()),
+                  );
+                },
+                child: const Text(
+                  "Skip >",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: ColorManager.primary,
+                  ),
+                ),
               ),
             ),
           ],
         ),
-        Positioned(
-          top: 50,
-          right: 20,
-          child: GestureDetector(
-            onTap: () {
-              Navigator.of(context).pushReplacement(
-                MaterialPageRoute(builder: (_) => const HomeView()),
-              );
-            },
-            child: Text(
-              "Skip >",
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
-            ),
-          ),
-        ),
-      ],
+      ),
     );
   }
 }
